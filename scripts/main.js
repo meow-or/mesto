00012d30705cf4439ad4e2134adddf44d2c2932c
meow-or <sttcx2014@yandex.ru>
2021-02-17 01:06:26 +0500

    feat: add create-form

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,22 @@
-let overlay = document.querySelector('.edit-form');
-let formOpenButton = document.querySelector('.profile__edit-button');
-let formCloseButton = overlay.querySelector('.edit-form__close');
-let formSaveButton = overlay.querySelector('.edit-form__submit-button');
-let form = overlay.querySelector('.edit-form__container');
+const overlay = document.querySelector('.edit-form');
+const formOpenButton = document.querySelector('.profile__edit-button');
+const formCloseButton = overlay.querySelector('.edit-form__close');
+const form = overlay.querySelector('.edit-form__container');
 
-let inputName = overlay.querySelector('.edit-form__input[name="name"]');
-let inputProfession = overlay.querySelector('.edit-form__input[name="profession"]');
+const inputName = overlay.querySelector('.edit-form__input[name="name"]');
+const inputProfession = overlay.querySelector('.edit-form__input[name="profession"]');
 
-let inputNameText = document.querySelector('.profile__title');
-let inputProfessionText = document.querySelector('.profile__subtitle');
+const inputNameText = document.querySelector('.profile__title');
+const inputProfessionText = document.querySelector('.profile__subtitle');
 
 
-let toggleForm = function () {
+const toggleForm = function () {
   overlay.classList.toggle('edit-form_opened');
   inputName.value = inputNameText.textContent;
   inputProfession.value = inputProfessionText.textContent;
 }
 
-let saveForm = function (evt) {
+const saveForm = function (evt) {
   evt.preventDefault();
   inputNameText.textContent = inputName.value;
   inputProfessionText.textContent = inputProfession.value;
