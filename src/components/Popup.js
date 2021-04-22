e25abf65fa833fb8a4dc7b53a0f6259f3f490f42
meow-or <sttcx2014@yandex.ru>
2021-04-22 10:36:00 +0500

    hotfix: critical comments - this._popupSelector > _popupElement rename

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,18 +1,18 @@
 export default class Popup {
   constructor (popupSelector) {
-    this._popupSelector = document.querySelector(popupSelector);
-    this._closeButton = this._popupSelector.querySelector('.popup__close');
+    this._popupElement = document.querySelector(popupSelector);
+    this._closeButton = this._popupElement.querySelector('.popup__close');
     this._handleEscClose = this._handleEscClose.bind(this);
   }
 
   open() {
-    this._popupSelector.classList.add('popup_opened');
+    this._popupElement.classList.add('popup_opened');
 
     document.addEventListener('keydown', this._handleEscClose);
   }
 
   close() {
-    this._popupSelector.classList.remove('popup_opened');
+    this._popupElement.classList.remove('popup_opened');
 
     document.removeEventListener('keydown', this._handleEscClose);
   }
@@ -30,7 +30,7 @@ export default class Popup {
       this.close();
     })
 
-    this._popupSelector.addEventListener('click', (evt) => {
+    this._popupElement.addEventListener('click', (evt) => {
       if(evt.target.classList.contains('popup_opened')) {
         this.close();
       }
